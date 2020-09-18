194de21aa1eec473dce0c2d7eb6e3341020cd39a
Dmitriy Borodiy <dmitriy.borodiy@gmail.com>
2020-09-17 17:30:26 -0700

    Breaking change: unified API with single method console.img

--- src/lib/console-extensions.ts
+++ src/lib/console-extensions.ts
@@ -1,8 +1,46 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+declare global {
+  interface Console {
+    img: (
+      source: string | ImageBitmap | OffscreenCanvas | HTMLCanvasElement,
+      scale?: number
+    ) => void;
+  }
+}
+
+type Dimensions = { readonly w: number; readonly h: number };
+
 /**
  * Adapted from dunxrion/console.image
  * Original created by Adrian Cooney
  */
 export const initConsoleLogImg = (console: Console) => {
+  /**
+   * Display an image in the console.
+   * @param  {any} source Source of the image: URI, Canvas, ImageBitmap, etc
+   * @param  {int} scale Scale factor on the image
+   * @return {null}
+   */
+  console.img = (
+    source: string | OffscreenCanvas | HTMLCanvasElement | ImageBitmap,
+    scale = 1
+  ) => {
+    if (typeof source === 'string') {
+      printFromImageUri(source, scale);
+    } else if (
+      source instanceof HTMLCanvasElement ||
+      source instanceof OffscreenCanvas
+    ) {
+      printFromCanvas(source, scale);
+    } else if (source instanceof ImageBitmap) {
+      printFromImageBitmap(source, scale);
+    } else {
+      throw new Error(
+        'unsupported source type, valid types are: string, Canvas or ImageBitmap'
+      );
+    }
+  };
+
   /**
    * Since the console.log doesn't respond to the `display` style,
    * setting a width and height has no effect. In fact, the only styles
@@ -27,13 +65,7 @@ export const initConsoleLogImg = (console: Console) => {
     };
   }
 
-  /**
-   * Display an image in the console.
-   * @param  {string} url The url of the image.
-   * @param  {int} scale Scale factor on the image
-   * @return {null}
-   */
-  console.image = (url: string, scale = 1) => {
+  const printFromImageUri = (url: string, scale = 1) => {
     const img = new Image();
 
     img.onload = () => {
@@ -58,7 +90,7 @@ export const initConsoleLogImg = (console: Console) => {
   /**
    * Snapshot a canvas context and output it to the console.
    */
-  console.screenshot = (
+  const printFromCanvas = (
     canvas: OffscreenCanvas | HTMLCanvasElement,
     scale = 1
   ) => {
@@ -96,58 +128,16 @@ export const initConsoleLogImg = (console: Console) => {
    * @param  {int} scale Scale factor on the image
    * @return {null}
    */
-  console.image = (url: string, scale = 1) => {
-    scale = scale || 1;
-    const img = new Image();
-
-    img.onload = () => {
-      const dim = getBox(img.width * scale, img.height * scale);
-      console.log(
-        '%c' + dim.string,
-        dim.style +
-          'background: url(' +
-          url +
-          '); background-size: ' +
-          img.width * scale +
-          'px ' +
-          img.height * scale +
-          'px;  background-size: 100% 100%; background-repeat: norepeat; color: transparent;'
-      );
-    };
-
-    img.src = url;
-  };
-
-  /**
-   * Display an image in the console.
-   * @param  {string} url The url of the image.
-   * @param  {int} scale Scale factor on the image
-   * @return {null}
-   */
-  console.bitmap = async (bitmap: ImageBitmap, scale = 1) => {
+  const printFromImageBitmap = async (bitmap: ImageBitmap, scale = 1) => {
     const canvas = createCanvas({ w: bitmap.width, h: bitmap.height });
     const ctx = canvas.getContext(
       'bitmaprenderer'
     ) as ImageBitmapRenderingContext;
     const bitmap2 = await createImageBitmap(bitmap);
     ctx.transferFromImageBitmap(bitmap2);
-    console.screenshot(ctx.canvas, scale);
+    console.img(ctx.canvas, scale);
   };
 };
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-declare global {
-  interface Console {
-    screenshot: (
-      canvas: OffscreenCanvas | HTMLCanvasElement,
-      scale?: number
-    ) => void;
-    bitmap: (bitmap: ImageBitmap, scale?: number) => void;
-    image: (src: string, scale?: number) => void;
-  }
-}
-
-type Dimensions = { readonly w: number; readonly h: number };
 
 const createCanvas = (size: Dimensions, elemId?: string): HTMLCanvasElement => {
   const canvas = document.createElement('canvas') as HTMLCanvasElement;
